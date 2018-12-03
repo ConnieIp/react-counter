a9dfe3c0cfe8dc823aee6ad1a292367ec8a810ab
Connie Ip <connie.ip@cargosmart.com>
2018-12-03 15:08:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={number:0}
+  increase=()=>{
+    this.setState({number:this.state.number+1})
+  }
+  decrease=()=>{
+    this.setState({number:this.state.number-1})
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.increase}>+</button>
+        <span>{this.state.number}</span>
+        <button onClick={this.decrease}>-</button>
       </div>
     );
   }
